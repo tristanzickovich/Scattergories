9f642e659cff68d781a45c126ffcccdc9532aa25
Tristan Zickovich <tzick002@ucr.edu>
2017-03-24 19:30:11 -0700

    added return to active game

--- home.php
+++ home.php
@@ -1,5 +1,14 @@
 <?php
 	include 'sessionStart.php';
+	$player = $user;
+	include 'connectdb.php';
+	$hasActiveGame = false; $activegid = 0;
+	$query = mysqli_query($db, "SELECT * FROM gameplayers WHERE player='$player'");
+	if(mysqli_num_rows($query) > 0){
+		$hasActiveGame = true;
+		$game = mysqli_fetch_array($query,MYSQLI_ASSOC);
+		$activegid = $game['gid'];
+	}
 ?>
 <html>
 	<head>
@@ -10,10 +19,18 @@
 	</head>
 	<body>
 		<h1 id="mainlogo">Scattergories</h1>
-		<?php print $user; ?>
+		<?php print $player; ?>
 		<a href='logout.php'>logout</a><br/>
-	   	<a href='viewlists.php'>View Lists</a>
-		<a href='setupgame.php'>Setup Game</a>
-		<a href='joinGame.php'>Join Game</a>
+		<?php 
+		if($hasActiveGame)
+			print '<a href="waitLobby.php?gid='.$activegid.'">Return To Game In Progress</a>';
+		else{
+		?>
+			<a href='viewlists.php'>View Lists</a>
+			<a href='setupgame.php'>Setup Game</a>
+			<a href='joinGame.php'>Join Game</a>
+		<?php
+		}
+		?>
 	</body>
 </html>
