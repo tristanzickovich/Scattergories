50bc7d64d56a9437704988c33d3ad6e9da594a56
Tristan Zickovich <tzick002@ucr.edu>
2017-04-20 20:18:43 -0700

    old file removal

--- header.php
+++
@@ -1,9 +0,0 @@
-<?php
-print'
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js"></script>
-<script type="text/javascript" src="general.js"></script>
-<link rel="stylesheet" type="text/css" href="style.css">
-'
-?>
